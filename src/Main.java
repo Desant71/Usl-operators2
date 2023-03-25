cae6febcf7324e31fe9d8cf0074d8bcdc6d493a9
Desant71 <desant31081992@gmail.com>
2023-03-26 02:11:33 +0300

    Add file2

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,32 @@
-import java.util.Scanner;
+import java.util.Scanner;//импорт сканер
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);//переменная сканер
+        int ochki = 0;//переменная для очков
+        while (true) {//цикл
+            System.out.println("Введите год в формате уууу");//вывод с.
+            int year = sc.nextInt();//переменная вводим год
+            System.out.println("Введите количество дней");
+            int dayIn = sc.nextInt();//перем. вводим день
+            int dayOfYear = 0;//переменная кол дгей в голду
 
-        while (true){
-            System.out.println("Введите год в формате уууу");
-            int year = sc.nextInt();
-
-            if(year % 400 == 0 ){
-                System.out.println("В году точно 366 дней");
-            }else if (year % 100 == 0){
-                System.out.println("В году точно 365 дней");
-            }else if (year % 4 == 0){
-                System.out.println("В году точно 366 дней");
-            }else if (year % 4 != 0){
-                System.out.println("В году точно 365 дней");
+            if (year % 400 == 0) {//если
+                dayOfYear = 366;
+            } else if (year % 100 == 0) {//иначе
+                dayOfYear = 365;
+            } else if (year % 4 == 0) {//иначе
+                dayOfYear = 366;
+            } else if (year % 4 != 0) {//иначе
+                dayOfYear = 365;
+            }
+            if (dayIn == dayOfYear) {//если
+                System.out.println("Правильно");//сообщение
+                ochki++;//плюсуем очки
+            } else {//иначе
+                System.out.println("Неправильно");
+                System.out.println("Количесво очков: " + ochki);
+                break;
             }
         }
     }
